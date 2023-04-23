14ea4ca6ac6176fc34ca3379600bea6efea821bd
ErnestMubatapasango <tawazzmubazz@gmail.com>
2023-04-23 15:21:27 +0200

    Task 0

---
+++ _printf.c
@@ -0,0 +1,51 @@
+#include "main.h"
+/**
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
+ *
+ */
+int _printf(const char *format, ...)
+{
+int count = 0;
+va_list args;
+va_start(args, format);
+
+while (*format != '\0')
+{
+if (*format == '%')
+{
+format++;
+if (*format == 'c')
+{
+char c = va_arg(args, int);
+putchar(c);
+count++;
+}
+else if (*format == 's')
+{
+char *s = va_arg(args, char *);
+while (*s != '\0')
+{
+putchar(*s);
+s++;
+count++;
+}
+}
+else if (*format == '%')
+{
+putchar('%');
+count++;
+}
+}
+else
+{
+putchar(*format);
+count++;
+}
+format++;
+}
+va_end(args);
+return (count);
+}
+
