67d0e4e1e7201a24cda9bc5435503057360bcb6f
ErnestMubatapasango <49388141+ErnestMubatapasango@users.noreply.github.com>
2023-04-24 11:11:02 +0200

    update - printf

--- _printf.c
+++ _printf.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
+void print_hedge(char hedge[], int *hed_ind);
 
 /**
  * _printf - Printf function
@@ -10,9 +10,9 @@ void print_buffer(char buffer[], int *buff_ind);
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
+	int flags, width, precision, size, hed_ind = 0;
 	va_list list;
-	char buffer[BUFF_SIZE];
+	char hedge[HED_SIZE];
 
 	if (format == NULL)
 		return (-1);
@@ -23,21 +23,21 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] != '%')
 		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
+			hedge[hed_ind++] = format[i];
+			if (hed_ind == HED_SIZE)
+				print_hedge(hedge, &hed_ind);
 			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
 		{
-			print_buffer(buffer, &buff_ind);
+			print_hedge(hedge, &hed_ind);
 			flags = get_flags(format, &i);
 			width = get_width(format, &i, list);
 			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
 			++i;
-			printed = handle_print(format, &i, list, buffer,
+			printed = handle_print(format, &i, list, hedge,
 				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
@@ -45,7 +45,7 @@ int _printf(const char *format, ...)
 		}
 	}
 
-	print_buffer(buffer, &buff_ind);
+	print_hedge(hedge, &hed_ind);
 
 	va_end(list);
 
@@ -53,15 +53,15 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
+ * print_hedge - Prints the contents of the buffer if it exist
+ * @hedge: Array of chars
+ * @hed_ind: Index at which to add next char, represents the length.
  */
-void print_buffer(char buffer[], int *buff_ind)
+void print_buffer(char hedge[], int *hed_ind)
 {
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
+	if (*hed_ind > 0)
+		write(1, &hedge[0], *hed_ind);
 
-	*buff_ind = 0;
+	*hed_ind = 0;
 }
 
